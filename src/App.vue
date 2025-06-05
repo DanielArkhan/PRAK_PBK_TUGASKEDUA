4ff507c1c515c6f2591d102ce9676aeddaa5d451
Daniel Arkhan <danielarkhan@student.uir.ac.id>
2025-06-05 15:51:43 +0700

    Commit kelima judul + layout awal halaman

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,19 @@
 <template>
-  <div>
-    Welcome to Coin Top-Up!
+  <div class="container">
+    <h1>Welcome to Coin Top-Up!</h1>
   </div>
 </template>
+
+<style>
+.container {
+  max-width: 800px;
+  margin: auto;
+  padding: 20px;
+}
+
+h1 {
+  color: #ff5c5c;
+  font-size: 32px;
+  margin-bottom: 20px;
+}
+</style>
