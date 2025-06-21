5e6f6f9c549944ced8ad17a62814ca19f43dd612
Daniel Arkhan <danielarkhan@student.uir.ac.id>
2025-06-21 17:05:54 +0700

    menambahkan content dari carousel

--- src/components/Carousel.vue
+++ src/components/Carousel.vue
@@ -24,16 +24,16 @@ export default {
     return {
       slides: [
         {
-          image: 'https://i.imgur.com/6U9r7CQ.jpg',
+          image: 'https://d2v6npc8wmnkqk.cloudfront.net/storage/tinymce/tfkKsKyJaawDVq3L1h2hE2H4jODTQ0lXWbqh4TDt.webp',
           caption: 'Diskon Spesial Minggu Ini!'
         },
         {
-          image: 'https://i.imgur.com/zRztKKM.jpg',
-          caption: 'Top Up Genshin Bonus 10%'
+          image: 'https://www.lapakgaming.com/blog/en-ph/wp-content/uploads/2025/06/LG-SEA-1448x520-HB-Genshin-SKIRK.png',
+          caption: 'Dapatkan Skirk dengan Top-up Welkin!'
         },
         {
-          image: 'https://i.imgur.com/6S0LgXc.jpg',
-          caption: 'Bayar Mudah Pakai QRIS'
+          image: 'https://pic.bittopup.com/apiUpload/ee9b12c63c475467ab54d5ecd67ecd07.webp?imageView2/0/w/2200/h/960/format/webp/ignore-error/1',
+          caption: 'Top-up PUBG MOBILE Sekarang!'
         }
       ]
     };
@@ -44,7 +44,7 @@ export default {
 <style scoped>
 .main-carousel {
   width: 100%;
-  max-width: 1000px;
+  max-width: 2000px;
   margin: 0 auto 24px auto;
   border-radius: 16px;
   overflow: hidden;
